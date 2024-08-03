426cf3955f269dce92a5ad5ebd80367faf6562cd
sandhya <145090559+sandhyaakkamgari@users.noreply.github.com>
2024-08-03 20:34:51 +0530

    Add files via upload

---
+++ singlelineandmultilinecomments.py
@@ -0,0 +1,17 @@
+#this is single line comment
+# print("Hello world")
+# print("Hello universe")
+# print("Hello everyone")
+
+print("Hello Sandhya")
+
+
+"""
+This is a multi-line comment with docstrings
+
+print("Hello world")
+print("Hello universe")
+print("Hello everyone")
+"""
+
+print("Hello Sandhya")
